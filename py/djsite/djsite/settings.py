9871ea975fa0aca2063bd8acc3a0f2b1084b5864
yanz <dirtysalt1987@gmail.com>
2016-01-04 12:49:48 +0800

    x

--- py/djsite/djsite/settings.py
+++ py/djsite/djsite/settings.py
@@ -171,8 +171,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -194,9 +194,9 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers':['file'],
+            'handlers': ['file'],
             'propagate': True,
-            'level':'DEBUG',
+            'level': 'DEBUG',
         },
         'ymuser': {
             'handlers': ['file'],
