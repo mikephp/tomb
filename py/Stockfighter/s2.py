51aaca7e8c8693aa3673fea188b4441ae2cc890e
yanz <dirtysalt1987@gmail.com>
2016-01-07 00:17:05 +0800

    x

--- py/Stockfighter/s2.py
+++ py/Stockfighter/s2.py
@@ -15,7 +15,7 @@ ACCOUNT = st['account']
 VENUE = st['venues'][0]
 STOCK = st['tickers'][0]
 ft = Stockfighter(VENUE, ACCOUNT)
-SLEEP = int(open('sleep', 'r').read())
+SLEEP = 3
 
 
 def xround(x): return x / 20 * 20
@@ -62,6 +62,9 @@ class OrderManager(object):
         # 最低成交价格. 最低成交价格有可能是当前市场价.
         deals = [x.price for x in filter(lambda o: o.diff, self.orders)]
         self.orders = filter(lambda o: o.filled < o.qty, self.orders)
+        price = 0 if not deals else deals[-1]  # min
+        # price = 0 if not deals else deals[0]  # max
+        return price
 
     def action(self):
         global STOCK
@@ -78,12 +81,11 @@ class OrderManager(object):
         # 根据当前市场投放新订单
         price_set = set(map(lambda o: o.price, self.orders))
         print('>>>>> price_set = %s' % (price_set))
-        for i in range(8):
-            p = self.market - i * 50
+        for i in range(4):
+            p = self.market - i * 40
             if p in price_set:
                 continue
-            # 每种价格只购买2000.
-            order = Order(p, 2000)
+            order = Order(p, 250)  # 只购买250, 小单.
 
             def place_new_order(order):
                 st = self.ft.order(STOCK, order.price,
@@ -143,27 +145,66 @@ def play_watch():
         time.sleep(1)
 
 
-def play_game():
+# 这种办法是假设不知道阈值是多少. 偶尔可以成功，但是加上阈值则会保险不少.
+# 尽可能地估计准确market price. 同样应该尽可能地多下单.
+def play_game1():
     global ft
     mw = MarketWatcher(ft)
     om = OrderManager(ft)
     pp = 0
-    THRESHOLD = int(open('threshold', 'r').read())
+    THRESHOLD = int(raw_input('target price > '))
     while True:
         print('====================')
-        om.update_order_status()
-        p = mw.estimate(om)
+        # 根据成交情况算出
+        p0 = om.update_order_status()
+        # 根据orderbook算出
+        p1 = mw.estimate(om)
+        if not p0 and not p1:
+            p = 0
+        elif p0 and p1:
+            p = min(p0, p1)
+        else:
+            p = p0 or p1
         if not p:
-            time.sleep(SLEEP)
-            continue
-        p = xround(p)
+            if pp:
+                p = pp
+            else:
+                p = THRESHOLD
         # 市场价格不能波动太大, 并且要小于某个阈值.
         if pp and p > pp:
-            p = min(pp + 50, p, THRESHOLD)
+            p = min(pp + 40, p)
+        p = min(p, THRESHOLD)
         pp = p
+
+        # 为了加快完成，需要让出部分profit.
+        margin = (THRESHOLD - p)
+        ratio = 0.4
+        p = int(margin * ratio) + p
+        p = xround(p)
         print('market price = %d' % p)
         om.update_market_price(p)
         time.sleep(SLEEP)
 
+
+# so effective...
+# 为了知道这个threshold, 可以先在web上放个market order, 买入10股，
+# 然后web界面就会显示target price. 只要下单足够快，价格就会保持在那个位置上.
+# 虽然可以很快完成交易，但是如果以target price来下单的话，cost则会相对较高，
+# 所以分数也就更低.
+def play_game2():
+    global STOCK
+    THRESHOLD = int(raw_input('target price > '))
+    price = THRESHOLD
+    size = 500
+    while True:
+        st = ft.order(STOCK, price, size, 'buy', 'limit')
+        tid = st['id']
+        while True:
+            st = ft.order_status(STOCK, tid)
+            filled = sum(map(lambda x: x['qty'], st['fills']))
+            if filled == size:
+                break
+            time.sleep(SLEEP)
+
 if __name__ == '__main__':
-    play_game()
+    play_game1()
