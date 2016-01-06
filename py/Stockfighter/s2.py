074d9ef44a3fe6a0b7714a60613885eed803a891
yanz <dirtysalt1987@gmail.com>
2016-01-06 14:24:13 +0800

    x

--- py/Stockfighter/s2.py
+++ py/Stockfighter/s2.py
@@ -5,6 +5,7 @@
 import gevent
 from gevent import monkey
 monkey.patch_all()
+from gevent.pool import Pool
 
 from stockfighter import Stockfighter
 import time
@@ -18,84 +19,112 @@ ft = Stockfighter(VENUE, ACCOUNT)
 
 class Order(object):
 
-    def __init__(self, tid, price, qty):
-        self.tid = tid
+    def __init__(self, price, qty):
+        self.tid = 0
         self.price = price
         self.qty = qty
-        self.invalid = False
+        self.filled = 0
 
 
 class OrderManager(object):
 
-    def __init__(self, ft, size=8)
+    def __init__(self, ft, max_size=8):
         self.orders = []
-        self.market_price = 0
-        self.size = size
+        self.market = 0
+        self.max_size = max_size
         self.ft = ft
+        self.step = 20
 
     def update_market_price(self, v):
-        self.market_price = v
+        self.market = (v / self.step) * self.step
+        print('market = %d' % (self.market))
         self.action()
 
-    def new_order(self, price, qty):
+    def action(self):
+        pool = Pool()
         global STOCK
-        st = self.ft.order(STOCK, price, qty, 'buy', 'limit')
-        tid = st['id']
-        order = Order(tid, price, qty)
-        self.orders.append(order)
 
-    def del_order(self, order):
-        global STOCK
-        tid = order.tid
-        self.ft.cancel(STOCK, tid)
-        order.invalid = True
+        # cancel all orders whose price is higher than market
+        cancel_orders = filter(lambda o: o.price > self.market, self.orders)
+        for o in cancel_orders:
+            g = gevent.spawn(self.ft.cancel, STOCK, o.tid)
+            pool.add(g)
+            self.orders.remove(o)
+        # check all orders which are filled and remove them.
+        for o in self.orders:
+            def check_filled_cond(order):
+                st = self.ft.order_status(STOCK, order.tid)
+                filled = sum(map(lambda x: x['qty'], st['fills']))
+                self.filled = filled
+            g = gevent.spawn(check_filled_cond, o)
+            pool.add(g)
+        pool.join()
+        self.orders = filter(lambda o: o.filled < o.qty, self.orders)
+
+        # place new orders according to market
+        price_set = set(map(lambda o: o.price, self.orders))
+        print('>>>>> price_set = {}'.format(price_set))
+        for i in range(10):
+            p = self.market - i * self.step
+            if p in price_set:
+                continue
+            order = Order(p, 1000)
+
+            def place_new_order(order):
+                st = self.ft.order(STOCK, order.price,
+                                   order.qty, 'buy', 'limit')
+                tid = st['id']
+                order.tid = tid
+            g = gevent.spawn(place_new_order, order)
+            pool.add(g)
+            self.orders.append(order)
+        pool.join()
+        price_set = set(map(lambda o: o.price, self.orders))
+        print('<<<<< price_set = {}'.format(price_set))
 
 
 class MarketWatcher(object):
 
     def __init__(self, ft):
         self.ft = ft
-        self.
+        self.market_price = 0
 
-    def action(self):
+    def watch(self, detail=False):
         global STOCK
-        quote = self.ft.quote(STOCK)
+        quote = ft.quote(STOCK)
+        if not ('bid' in quote and 'ask' in quote):
+            return self.market_price
+        if detail:
+            print('bid: %d(+%d) @ %d, ask: %d(+%d) @ %d' % (
+                quote['bidSize'], quote['bidDepth'], quote['bid'],
+                quote['askSize'], quote['askDepth'], quote['ask']))
+        bid = quote['bid']
+        if not self.market_price:
+            self.market_price = bid
+        elif self.market_price > bid:
+            self.market_price = bid
+        else:
+            self.market_price = min(self.market_price + 100, bid)
+        return self.market_price
 
 
-def play_watcher():
-    quote = ft.quote(STOCK)
-    if not ('bid' in quote and 'ask' in quote and 'last' in quote):
-        continue
-    print('bid: %d(+%d) @ %d, ask: %d(+%d) @ %d, ok: %d @ %d' % (
-        quote['bidSize'], quote['bidDepth'], quote['bid'], quote['askSize'],
-        quote['askDepth'], quote['ask'], quote['lastSize'], quote['last']))
-    time.sleep(1)
-
-P = 5386
-while True:
-    quote = ft.quote(STOCK)
-    if not ('bid' in quote and 'ask' in quote):
+def play_watch():
+    global ft
+    mw = MarketWatcher(ft)
+    while True:
+        mw.watch(detail=True)
         time.sleep(1)
-        continue
-    bid = quote['bid']
-    if not P:
-        P = bid
-    if P > bid:
-        P = bid
-    else:
-        P = int((bid - P) * 0.2 + P)
-    size = 10000
-    timeout = 5
-    price = P
-    print 'place limit order: %d @ %d' % (size, price)
-    st = ft.order(STOCK, price, size, 'buy', 'limit')
-    tid = st['id']
-    time.sleep(timeout)
-    ft.cancel(STOCK, tid)
-    st = ft.order_status(STOCK, tid)
-    v = sum(map(lambda x: x['qty'], st['fills']))
-    p = sum(map(lambda x: x['qty'] * x['price'], st['fills']))
-    if v:
-        P -= 50
-    else:
-        P += 20
+
+
+def play_game():
+    global ft
+    mw = MarketWatcher(ft)
+    om = OrderManager(ft)
+    while True:
+        p = mw.watch()
+        if not p:
+            time.sleep(1)
+        om.update_market_price(p)
+
+if __name__ == '__main__':
+    play_game()
