ed60a89a6229b422b28362c95356e5dcad390d2c
yanz <dirtysalt1987@gmail.com>
2016-01-12 10:30:53 +0800

    x

--- py/qstk-hw/hw3.py
+++ py/qstk-hw/hw3.py
@@ -24,8 +24,7 @@ def simulate(orders, amount):
     ldt_timestamps = du.getNYSEdays(
         dt_start, dt_end + dt.timedelta(hours=24), dt_timeofday)
     c_dataobj = da.DataAccess('Yahoo')
-    ls_keys = ['close']
-    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ['close'])
     df_price = ldf_data[0]
     cash = amount
     stocks = {}
@@ -45,15 +44,51 @@ def simulate(orders, amount):
                     stocks[sym] = qty
             else:
                 cash += price * qty
-                stocks[sym] -= qty
+                if sym in stocks:
+                    stocks[sym] -= qty
+                else:
+                    stocks[sym] = -qty
             order_idx += 1
         total = cash
         for sym in stocks.keys():
             qty = stocks[sym]
             price = df_price[sym].ix[ts]
             total += qty * price
-        values.append((date, total))
-    return values
+        values.append(total)
+    return (ldt_timestamps, np.array(values))
+
+
+def compare_to_SPX(ldt_timestamps, values):
+    c_dataobj = da.DataAccess('Yahoo')
+    ldf_data = c_dataobj.get_data(ldt_timestamps, ['$SPX'], ['close'])
+    spx_values = ldf_data[0].values
+    values = np.expand_dims(values, axis=1)
+    prices = np.hstack((values, spx_values))
+    na_normalized_prices = prices / prices[0, :]
+
+    def stock_stat(prices):
+        na_rets = tsu.returnize0(prices)
+        N = 252
+        std = np.std(na_rets, axis=0)
+        mean = np.mean(na_rets, axis=0)
+        sr = np.sqrt(N) * mean / std
+        total = np.cumprod(na_rets + 1, axis=0)[-1, :]
+        return (std, mean, sr, total)
+
+    (std, mean, sr, total) = stock_stat(na_normalized_prices)
+    print('Date Range: %s to %s' % (ldt_timestamps[0], ldt_timestamps[-1]))
+    print('Sharpe Ratio of Fund and $SPX: %s' % (sr))
+    print('Total Return of Fund and $SPX: %s' % (total))
+    print('Standard Deviation of Fund and $SPX: %s' % (std))
+    print('Average Daily Return of Fund and $SPX: %s' % (mean))
+
+    plt.clf()
+    N = na_normalized_prices.shape[0]
+    plt.plot(np.arange(N), na_normalized_prices)
+    plt.legend(['Fund', '$SPX'], loc='upper left')
+    plt.ylabel('Normalized Prices')
+    plt.xlabel('Time')
+    plt.savefig('hw3.pdf', format='pdf')
 
 
 def read_orders(fname):
@@ -63,18 +98,22 @@ def read_orders(fname):
             (year, mon, day, sym, direction, qty, _) = s.strip().split(',')
             date = dt.datetime(int(year), int(mon), int(day))
             orders.append((date, sym, direction, int(qty)))
+        orders.sort(lambda x, y: cmp(x[0], y[0]))
+        # print orders
         return orders
 
 
-def write_values(values, fname):
-    with open(fname, 'w') as fh:
-        lines = map(lambda x: '%d,%d,%d,%d\n' %
-                    (x[0].year, x[0].month, x[0].day, x[1]), values)
-        fh.writelines(lines)
-
+def output_values(ldt_timestamps, values):
+    rs = zip(ldt_timestamps, values)
+    lines = map(lambda x: '%d,%d,%d,%d' %
+               (x[0].year, x[0].month, x[0].day, x[1]), rs)
+    for x in lines:
+        print(x)
 
 if __name__ == '__main__':
-    orders = read_orders('orders-short.csv')
-    # print(orders)
-    values = simulate(orders, 1000000)
-    write_values(values, 'values-short2.csv')
+    # orders = read_orders('orders-short.csv')
+    # orders = read_orders('orders.csv')
+    orders = read_orders('orders2.csv')
+    (ldt_timestamps, values) = simulate(orders, 1000000)
+    output_values(ldt_timestamps, values)
+    compare_to_SPX(ldt_timestamps, values)
