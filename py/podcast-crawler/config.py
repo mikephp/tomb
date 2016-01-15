6a374f69695de18a0f70b5fe3708492a46aafa4f
yanz <dirtysalt1987@gmail.com>
2016-01-15 15:46:48 +0800

    x

--- py/podcast-crawler/config.py
+++ py/podcast-crawler/config.py
@@ -17,3 +17,33 @@ try:
     from local_config import *
 except:
     pass
+
+ITUNES_COUNTRY_CODE = {
+    'Canada': 'ca', 'Uruguay (English)': 'uy_en', 'Turkmenistan': 'tm', 'Lithuania': 'lt', 'Cambodia': 'kh', 'Germany (English)': 'de_en', 'Turkey (English)': 'tr_en', 'Micronesia': 'fm', 'Argentina': 'ar', 'Bolivia': 'bo', 'Burkina Faso': 'bf', 'Ghana': 'gh', 'Saudi Arabia': 'sa', 'Panama (English)': 'pa_en', 'Dominican Republic (English)': 'do_en', 'Japan': 'jp', 'Cape Verde': 'cv', 'Slovenia': 'si', 'Guatemala': 'gt', 'Zimbabwe': 'zw', 'Belize (Spanish)': 'bz_es', 'Jordan': 'jo', 'St. Lucia': 'lc', 'Dominica': 'dm', 'Liberia': 'lr', 'Netherlands': 'nl', 'Russia (English)': 'ru_en', 'Pakistan': 'pk', 'Netherlands (English)': 'nl_en', 'Tanzania': 'tz', 'Vietnam': 'vn', 'S\xc3\xa3o Tom\xc3\xa9 and Pr\xc3\xadncipe': 'st', 'Dominica (English)': 'dm_en', 'Mauritania': 'mr', 'Seychelles': 'sc', 'New Zealand': 'nz', 'Yemen': 'ye', 'Jamaica': 'jm', 'Malaysia (English)': 'my_en', 'Albania': 'al', 'Macau': 'mo', 'Korea (English)': 'kr_en', 'India': 'in', 'Azerbaijan': 'az', 'United Arab Emirates': 'ae', 'Kenya': 'ke', 'Tajikistan': 'tj', 'Nicaragua (English)': 'ni_en', 'Turkey': 'tr', 'Japan (English)': 'jp_en', 'Czech Republic': 'cz', 'Luxembourg (English)': 'lu_en', 'Solomon Islands': 'sb', 'Switzerland (French)': 'ch_fr', 'Palau': 'pw', 'Mongolia': 'mn', 'France': 'fr', 'Bermuda': 'bm', 'Slovakia': 'sk', 'Sweden (English)': 'se_en', 'Peru': 'pe', 'Indonesia (English)': 'id_en', 'Norway': 'no', 'Malawi': 'mw', 'Benin': 'bj', 'Macau (English)': 'mo_en', 'Brazil (English)': 'br_en', 'Singapore': 'sg', 'China': 'cn', 'Armenia': 'am', 'Suriname (English)': 'sr_en', 'Dominican Republic': 'do', 'Luxembourg (French)': 'lu_fr', 'Hong Kong (English)': 'hk_en', 'Ukraine': 'ua', 'Bahrain': 'bh', 'Cayman Islands': 'ky', 'Portugal (English)': 'pt_en', 'Finland': 'fi', 'Mauritius': 'mu', 'Sweden': 'se', 'Belarus': 'by', 'British Virgin Islands': 'vg', 'Mali': 'ml', 'Russia': 'ru', 'Bulgaria': 'bg', 'United States': 'us', 'Romania': 'ro', 'Angola': 'ao', 'Chad': 'td', 'China (English)': 'cn_en', 'Cyprus': 'cy', 'Brunei Darussalam': 'bn', 'Qatar': 'qa', 'Malaysia': 'my', 'Austria': 'at', 'Latvia': 'lv', 'Mozambique': 'mz', 'Uganda': 'ug', 'Hungary': 'hu', 'Niger': 'ne', 'El Salvador (English)': 'sv_en', 'Brazil': 'br',
+    'Costa Rica (English)': 'cr_en', 'Singapore (English)': 'sg_en', 'Kuwait': 'kw', 'Panama': 'pa', 'Costa Rica': 'cr', 'Luxembourg': 'lu', 'St. Kitts and Nevis': 'kn', 'Bahamas': 'bs', 'Ireland': 'ie', 'Italy (English)': 'it_en', 'Italy': 'it', 'Nigeria': 'ng', 'Taiwan (English)': 'tw_en', 'Ecuador': 'ec', 'Australia': 'au', 'Algeria': 'dz', 'El Salvador': 'sv', 'Finland (English)': 'fi_en', 'Argentina (English)': 'ar_en', 'Turks and Caicos': 'tc', 'Chile': 'cl', 'Belgium': 'be', 'Thailand': 'th', 'Belgium (English)': 'be_en', 'Hong Kong': 'hk', 'Sierra Leone': 'sl', 'Switzerland (Italian)': 'ch_it', 'Oman': 'om', 'St. Vincent and The Grenadines': 'vc', 'Gambia': 'gm', 'Philippines': 'ph', 'Uzbekistan': 'uz', 'Moldova': 'md', 'Paraguay (English)': 'py_en', 'Croatia': 'hr', 'Guatemala (English)': 'gt_en', 'Guinea-Bissau': 'gw', 'Switzerland': 'ch', 'Grenada': 'gd', 'Spain (English)': 'es_en', 'Belize': 'bz', 'Portugal': 'pt', 'Estonia': 'ee', 'Uruguay': 'uy', 'South Africa': 'za', 'Lebanon': 'lb', 'France (English)': 'fr_en', 'Tunisia': 'tn', 'United States (Spanish)': 'us_es', 'Antigua and Barbuda': 'ag', 'Spain': 'es', 'Colombia': 'co', 'Norway (English)': 'no_en', 'Vietnam (English)': 'vn_en', 'Taiwan': 'tw', 'Fiji': 'fj', 'Barbados': 'bb', 'Madagascar': 'mg', 'Belgium (French)': 'be_fr', 'Bhutan': 'bt', 'Nepal': 'np', 'Malta': 'mt', 'Honduras (English)': 'hn_en', 'Chile (English)': 'cl_en', 'Suriname': 'sr', 'Anguilla': 'ai', 'Venezuela': 've', 'Swaziland': 'sz', 'Israel': 'il', 'Lao': 'la', 'Indonesia': 'id', 'Iceland': 'is', 'Canada (French)': 'ca_fr', 'Senegal': 'sn', 'Papua New Guinea': 'pg', 'Thailand (English)': 'th_en', 'Trinidad and Tobago': 'tt', 'Germany': 'de', 'Denmark': 'dk', 'Kazakhstan': 'kz', 'Poland': 'pl', 'Ecuador (English)': 'ec_cn', 'Kyrgyzstan': 'kg', 'Montserrat': 'ms', 'Macedonia': 'mk', 'Mexico (English)': 'mx_en', 'Sri Lanka': 'lk', 'Korea': 'kr', 'Guyana': 'gy', 'Colombia (English)': 'co_en', 'Venezuela (English)': 've_en', 'Honduras': 'hn', 'Mexico': 'mx', 'Egypt': 'eg', 'Nicaragua': 'ni', 'Denmark (English)': 'dk_en', 'Switzerland (English)': 'ch_en', 'Austria (English)': 'at_en', 'United Kingdom': 'gb', 'Congo': 'cg', 'Greece': 'gr', 'Paraguay': 'py', 'Namibia': 'na', 'Bolivia (English)': 'bo_en', 'Botswana': 'bw'}
+
+if TEST_ON_US:
+    ITUNES_COUNTRY_CODE = {'United States': 'us', 'China': 'cn'}
+
+
+ITUNES_PODCAST_GENRE_CODE = {
+    'Arts': 1301,
+    'Business': 1321,
+    'Comedy': 1303,
+    'Education': 1304,
+    'Games & Hobbies': 1323,
+    'Government & Organizations': 1325,
+    'Health': 1307,
+    'Kids & Family': 1305,
+    'Music': 1310,
+    'News & Politics': 1311,
+    'Religion & Spirituality': 1314,
+    'Science & Medicine': 1315,
+    'Society & Culture': 1324,
+    'Sports & Recreation': 1316,
+    'TV & Film': 1309,
+    'Technology': 1318
+}
+
+ITUNES_PODCAST_ALL_GENRE_CODE = {'Arts': 1301, 'Regional': 1474, 'Natural Sciences': 1477, 'Fitness & Nutrition': 1417, 'Podcasting': 1450, 'Games & Hobbies': 1323, 'Science & Medicine': 1315, 'Performing Arts': 1405, 'Sports & Recreation': 1316, 'Christianity': 1439, 'Spirituality': 1444, 'Kids & Family': 1305, 'Other': 1464, 'Music': 1310, 'Educational Technology': 1468, 'Medicine': 1478, 'Tech News': 1448, 'Comedy': 1303, 'Local': 1475, 'Social Sciences': 1479, 'Careers': 1410, 'Personal Journals': 1302, 'Philosophy': 1443, 'Literature': 1401, 'Shopping': 1472, 'Business': 1321, 'Food': 1306, 'Automotive': 1454, 'Video Games': 1404, 'Language Courses': 1469, 'Investing': 1412, 'Software How-To': 1480, 'College & High School': 1466,
+                                 'Aviation': 1455, 'Non-Profit': 1476, 'Training': 1470, 'Higher Education': 1416, 'Judaism': 1441, 'Government & Organizations': 1325, 'Business News': 1471, 'Religion & Spirituality': 1314, 'K-12': 1415, 'Alternative Health': 1481, 'News & Politics': 1311, 'Podcasts': 26, 'Places & Travel': 1320, 'Sexuality': 1421, 'Other Games': 1461, 'Self-Help': 1420, 'Amateur': 1467, 'Professional': 1465, 'TV & Film': 1309, 'History': 1462, 'Hinduism': 1463, 'Visual Arts': 1406, 'Fashion & Beauty': 1459, 'Outdoor': 1456, 'Management & Marketing': 1413, 'Society & Culture': 1324, 'National': 1473, 'Buddhism': 1438, 'Gadgets': 1446, 'Design': 1402, 'Hobbies': 1460, 'Health': 1307, 'Education': 1304, 'Technology': 1318, 'Islam': 1440}
--- py/podcast-crawler/itunes.py
+++ py/podcast-crawler/itunes.py
@@ -10,32 +10,6 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 from config import *
 
-ITUNES_COUNTRY_CODE = {
-    'Canada': 'ca', 'Uruguay (English)': 'uy_en', 'Turkmenistan': 'tm', 'Lithuania': 'lt', 'Cambodia': 'kh', 'Germany (English)': 'de_en', 'Turkey (English)': 'tr_en', 'Micronesia': 'fm', 'Argentina': 'ar', 'Bolivia': 'bo', 'Burkina Faso': 'bf', 'Ghana': 'gh', 'Saudi Arabia': 'sa', 'Panama (English)': 'pa_en', 'Dominican Republic (English)': 'do_en', 'Japan': 'jp', 'Cape Verde': 'cv', 'Slovenia': 'si', 'Guatemala': 'gt', 'Zimbabwe': 'zw', 'Belize (Spanish)': 'bz_es', 'Jordan': 'jo', 'St. Lucia': 'lc', 'Dominica': 'dm', 'Liberia': 'lr', 'Netherlands': 'nl', 'Russia (English)': 'ru_en', 'Pakistan': 'pk', 'Netherlands (English)': 'nl_en', 'Tanzania': 'tz', 'Vietnam': 'vn', 'S\xc3\xa3o Tom\xc3\xa9 and Pr\xc3\xadncipe': 'st', 'Dominica (English)': 'dm_en', 'Mauritania': 'mr', 'Seychelles': 'sc', 'New Zealand': 'nz', 'Yemen': 'ye', 'Jamaica': 'jm', 'Malaysia (English)': 'my_en', 'Albania': 'al', 'Macau': 'mo', 'Korea (English)': 'kr_en', 'India': 'in', 'Azerbaijan': 'az', 'United Arab Emirates': 'ae', 'Kenya': 'ke', 'Tajikistan': 'tj', 'Nicaragua (English)': 'ni_en', 'Turkey': 'tr', 'Japan (English)': 'jp_en', 'Czech Republic': 'cz', 'Luxembourg (English)': 'lu_en', 'Solomon Islands': 'sb', 'Switzerland (French)': 'ch_fr', 'Palau': 'pw', 'Mongolia': 'mn', 'France': 'fr', 'Bermuda': 'bm', 'Slovakia': 'sk', 'Sweden (English)': 'se_en', 'Peru': 'pe', 'Indonesia (English)': 'id_en', 'Norway': 'no', 'Malawi': 'mw', 'Benin': 'bj', 'Macau (English)': 'mo_en', 'Brazil (English)': 'br_en', 'Singapore': 'sg', 'China': 'cn', 'Armenia': 'am', 'Suriname (English)': 'sr_en', 'Dominican Republic': 'do', 'Luxembourg (French)': 'lu_fr', 'Hong Kong (English)': 'hk_en', 'Ukraine': 'ua', 'Bahrain': 'bh', 'Cayman Islands': 'ky', 'Portugal (English)': 'pt_en', 'Finland': 'fi', 'Mauritius': 'mu', 'Sweden': 'se', 'Belarus': 'by', 'British Virgin Islands': 'vg', 'Mali': 'ml', 'Russia': 'ru', 'Bulgaria': 'bg', 'United States': 'us', 'Romania': 'ro', 'Angola': 'ao', 'Chad': 'td', 'China (English)': 'cn_en', 'Cyprus': 'cy', 'Brunei Darussalam': 'bn', 'Qatar': 'qa', 'Malaysia': 'my', 'Austria': 'at', 'Latvia': 'lv', 'Mozambique': 'mz', 'Uganda': 'ug', 'Hungary': 'hu', 'Niger': 'ne', 'El Salvador (English)': 'sv_en', 'Brazil': 'br',
-    'Costa Rica (English)': 'cr_en', 'Singapore (English)': 'sg_en', 'Kuwait': 'kw', 'Panama': 'pa', 'Costa Rica': 'cr', 'Luxembourg': 'lu', 'St. Kitts and Nevis': 'kn', 'Bahamas': 'bs', 'Ireland': 'ie', 'Italy (English)': 'it_en', 'Italy': 'it', 'Nigeria': 'ng', 'Taiwan (English)': 'tw_en', 'Ecuador': 'ec', 'Australia': 'au', 'Algeria': 'dz', 'El Salvador': 'sv', 'Finland (English)': 'fi_en', 'Argentina (English)': 'ar_en', 'Turks and Caicos': 'tc', 'Chile': 'cl', 'Belgium': 'be', 'Thailand': 'th', 'Belgium (English)': 'be_en', 'Hong Kong': 'hk', 'Sierra Leone': 'sl', 'Switzerland (Italian)': 'ch_it', 'Oman': 'om', 'St. Vincent and The Grenadines': 'vc', 'Gambia': 'gm', 'Philippines': 'ph', 'Uzbekistan': 'uz', 'Moldova': 'md', 'Paraguay (English)': 'py_en', 'Croatia': 'hr', 'Guatemala (English)': 'gt_en', 'Guinea-Bissau': 'gw', 'Switzerland': 'ch', 'Grenada': 'gd', 'Spain (English)': 'es_en', 'Belize': 'bz', 'Portugal': 'pt', 'Estonia': 'ee', 'Uruguay': 'uy', 'South Africa': 'za', 'Lebanon': 'lb', 'France (English)': 'fr_en', 'Tunisia': 'tn', 'United States (Spanish)': 'us_es', 'Antigua and Barbuda': 'ag', 'Spain': 'es', 'Colombia': 'co', 'Norway (English)': 'no_en', 'Vietnam (English)': 'vn_en', 'Taiwan': 'tw', 'Fiji': 'fj', 'Barbados': 'bb', 'Madagascar': 'mg', 'Belgium (French)': 'be_fr', 'Bhutan': 'bt', 'Nepal': 'np', 'Malta': 'mt', 'Honduras (English)': 'hn_en', 'Chile (English)': 'cl_en', 'Suriname': 'sr', 'Anguilla': 'ai', 'Venezuela': 've', 'Swaziland': 'sz', 'Israel': 'il', 'Lao': 'la', 'Indonesia': 'id', 'Iceland': 'is', 'Canada (French)': 'ca_fr', 'Senegal': 'sn', 'Papua New Guinea': 'pg', 'Thailand (English)': 'th_en', 'Trinidad and Tobago': 'tt', 'Germany': 'de', 'Denmark': 'dk', 'Kazakhstan': 'kz', 'Poland': 'pl', 'Ecuador (English)': 'ec_cn', 'Kyrgyzstan': 'kg', 'Montserrat': 'ms', 'Macedonia': 'mk', 'Mexico (English)': 'mx_en', 'Sri Lanka': 'lk', 'Korea': 'kr', 'Guyana': 'gy', 'Colombia (English)': 'co_en', 'Venezuela (English)': 've_en', 'Honduras': 'hn', 'Mexico': 'mx', 'Egypt': 'eg', 'Nicaragua': 'ni', 'Denmark (English)': 'dk_en', 'Switzerland (English)': 'ch_en', 'Austria (English)': 'at_en', 'United Kingdom': 'gb', 'Congo': 'cg', 'Greece': 'gr', 'Paraguay': 'py', 'Namibia': 'na', 'Bolivia (English)': 'bo_en', 'Botswana': 'bw'}
-
-if TEST_ON_US:
-    ITUNES_COUNTRY_CODE = {'United States': 'us'}
-
-ITUNES_PODCAST_GENRE_CODE = {
-    'Arts': 1301,
-    'Business': 1321,
-    'Comedy': 1303,
-    'Education': 1304,
-    'Games & Hobbies': 1323,
-    'Government & Organizations': 1325,
-    'Health': 1307,
-    'Kids & Family': 1305,
-    'Music': 1310,
-    'News & Politics': 1311,
-    'Religion & Spirituality': 1314,
-    'Science & Medicine': 1315,
-    'Society & Culture': 1324,
-    'Sports & Recreation': 1316,
-    'TV & Film': 1309,
-    'Technology': 1318
-}
-
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import *
@@ -61,6 +35,7 @@ class Podcast(Base):
     cover30 = Column(TEXT)
     releaseDate = Column(DateTime)
     trackCount = Column(Integer)
+    genreText = Column(TEXT)
     skip = Column(Integer)  # should we skip it?
     wellFormed = Column(Integer)  # content of feedUrl is well-formed?
     parsedDate = Column(DateTime)  # when this feedUrl is parsed.
@@ -238,7 +213,8 @@ def collect_genres_1(country, genre, store):
     session = Session()
     rs = [u for u in session.query(Podcast).filter_by(
         country=country, genre=genre)]
-    pool = Pool(size=32)
+    pool = Pool(size=8)
+    print('COLLECT GENRES ON (%s, %d)' % (country, genre))
 
     def f(r):
         pid = r.pid
@@ -253,15 +229,13 @@ def collect_genres_1(country, genre, store):
             if json['resultCount'] != 0:
                 print('PARSE LOOKUP FAILED. result count %d. pid = %d' %
                       (json['resultCount'], pid))
-            session.add(r)
-            sesson.commit()
             return
         data = json['results'][0]
         genreIds = map(lambda x: int(x), data['genreIds'])
         genres = data['genres']
         for (idx, genreId) in enumerate(genreIds):
             genre = genres[idx]
-            store[genreId] = genre
+            store[genre] = genreId
     for r in rs:
         g = gevent.spawn(f, r)
         pool.add(g)
@@ -271,8 +245,7 @@ def collect_genres_1(country, genre, store):
 def collect_genres(comb):
     store = {}
     func_on_comb(comb, collect_genres_1, store)
-    for k in store.keys():
-        print k, store[k]
+    print(store)
 
 
 def parse_lookup_1(country, genre):
@@ -296,7 +269,7 @@ def parse_lookup_1(country, genre):
                 print('PARSE LOOKUP FAILED. result count %d. pid = %d' %
                       (json['resultCount'], pid))
             session.add(r)
-            sesson.commit()
+            session.commit()
             return
         data = json['results'][0]
         # some issues when comparing datetime.
@@ -306,7 +279,7 @@ def parse_lookup_1(country, genre):
             # print('PARSE LOOKUP CACHED. pid = %d' % (pid))
             return
         # genreIds = map(lambda x: int(x), data['genreIds'])
-        # genres = data['genres']
+        genres = data['genres']
         if 'artworkUrl30' in data:
             r.cover30 = data['artworkUrl30']
         if 'releaseDate' in data:
@@ -319,6 +292,8 @@ def parse_lookup_1(country, genre):
             r.authorId = data['artistId']
         if 'artistName' in data:
             r.authorName = data['artistName'].encode('utf-8')
+        if 'genres' in data:
+            r.genreText = (','.join(data['genres'])).encode('utf-8')
         session.add(r)
         session.commit()
 
@@ -365,7 +340,7 @@ def down_feed_1(country, genre):
             session.add(r)
             return
         value = res.text.encode('utf-8')
-        if len(value) > (1 << 23):
+        if len(value) > (1 << 21):
             r.skip = 1
             session.add(r)
             return
@@ -455,10 +430,13 @@ def parse_feed_1(country, genre):
 
 if __name__ == '__main__':
     comb = make_combination()
-    # down_index(comb)
-    # func_on_comb(comb, parse_index_1)
-    # func_on_comb(comb, down_lookup_1)
-    # func_on_comb(comb, parse_lookup_1)
-    # func_on_comb(comb, down_feed_1)
-    # func_on_comb(comb, parse_feed_1)
-    collect_genres(comb)
+    # collect_genres(comb)
+    if 'index' in sys.argv:
+        down_index(comb)
+        func_on_comb(comb, parse_index_1)
+    if 'lookup' in sys.argv:
+        func_on_comb(comb, down_lookup_1)
+        func_on_comb(comb, parse_lookup_1)
+    if 'feed' in sys.argv:
+        func_on_comb(comb, down_feed_1)
+        func_on_comb(comb, parse_feed_1)
