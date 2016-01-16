0e93ac3a6c076b192eaa5ff0f68bf95805068e29
yanz <dirtysalt1987@gmail.com>
2016-01-16 13:36:21 +0800

    x

--- ios/YGYTPlayer/YGYTServer/app.py
+++ ios/YGYTPlayer/YGYTServer/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
+# coding:utf-8
+# Copyright (C) dirlt
 
 import gevent
 import web
@@ -15,6 +15,7 @@ urls = ("/index/", 'index',
 
 app = web.application(urls, globals())
 
+
 def viewcount_in_string(v):
     s = ''
     while(v >= 1000):
@@ -26,33 +27,49 @@ def viewcount_in_string(v):
 LANG_MAPPING_V1 = ('', 'en', 'fr', 'de', 'ja', 'pt', 'es', 'it', 'zh', 'ru',)
 LANG_MAPPING_V2 = ('', 'en', 'ja', 'pt', 'zh', 'ru',)
 
+
 class index:
+
     def GET(self):
-        ctx = web.input(s = 0, sid = '', c = 20, lang = 0, v = 1, q = '')
+        ctx = web.input(s=0, sid='', c=20, lang=0, v=1, q='')
         # print web.ctx.path, web.ctx.query
-        s = int(ctx.s); sid = ctx.sid; c = int(ctx.c); lang_idx = int(ctx.lang); version = int(ctx.v); q = ctx.q
+        s = int(ctx.s)
+        sid = ctx.sid
+        c = int(ctx.c)
+        lang_idx = int(
+            ctx.lang)
+        version = int(ctx.v)
+        q = ctx.q
         # skip + limit
         # 性能不行再使用>sid & limit
         query = {}
         if version == 1:
-            lang = '' if lang_idx > len(LANG_MAPPING_V1) else LANG_MAPPING_V1[lang_idx]
-            if lang: query['lang'] = lang
+            lang = '' if lang_idx > len(
+                LANG_MAPPING_V1) else LANG_MAPPING_V1[lang_idx]
+            if lang:
+                query['lang'] = lang
         elif version == 2:
-            lang = '' if lang_idx > len(LANG_MAPPING_V2) else LANG_MAPPING_V2[lang_idx]
-            if lang == 'en': query['lang'] = {'$in': ['en', 'fr', 'de', 'es', 'it']}
-            elif lang: query['lang'] = lang
+            lang = '' if lang_idx > len(
+                LANG_MAPPING_V2) else LANG_MAPPING_V2[lang_idx]
+            if lang == 'en':
+                query['lang'] = {'$in': ['en', 'fr', 'de', 'es', 'it']}
+            elif lang:
+                query['lang'] = lang
         # print('query on mongo = %s' % query)
-        projection_list = ['title', 'description', 'viewcount', 'thumb', 'key', '_id', 'bigthumbhd']
+        projection_list = [
+            'title', 'description', 'viewcount', 'thumb', 'key', '_id', 'bigthumbhd']
         sort_pred = [('viewcount', pymongo.DESCENDING)]
         if q:
             query['$text'] = {'$search': q}
             projection_dict = {}
-            for x in projection_list: projection_dict[x] = True
+            for x in projection_list:
+                projection_dict[x] = True
             projection_dict['score'] = {'$meta': 'textScore'}
             sort_pred.insert(0, ('score', {'$meta': 'textScore'}))
         else:
             projection_dict = projection_list
-        rs = CF.FACT_TABLE.find(query, skip = s, limit = c, projection = projection_dict, sort = sort_pred)
+        rs = CF.FACT_TABLE.find(
+            query, skip=s, limit=c, projection=projection_dict, sort=sort_pred)
         vds = []
         for r in rs:
             vd = {}
@@ -70,11 +87,13 @@ class index:
         web.header('Content-Type', 'application/json')
         return json.dumps(js)
 
+
 class detail:
+
     def GET(self):
-        ctx = web.input(id = '')
+        ctx = web.input(id='')
         vid = ctx.id
-        r = CF.FACT_TABLE.find_one({'key': vid}, projection = ['videos'])
+        r = CF.FACT_TABLE.find_one({'key': vid}, projection=['videos'])
         ss = r['videos']
         vds = []
         for r in ss:
@@ -90,10 +109,12 @@ class detail:
         web.header('Content-Type', 'application/json')
         return json.dumps(js)
 
+
 class popular:
+
     def GET(self):
-        js = {'terms': ['yoga with adriene', 'uchiha itachi', 'uchiha madara',
-                        'best sex with yoga', 'free ponography']}
+        js = {
+            'terms': ['yoga with adriene', 'best sex with yoga', 'free ponography']}
         web.header('Content-Type', 'application/json')
         return json.dumps(js)
 
--- ios/YGYTPlayer/YGYTServer/util.py
+++ ios/YGYTPlayer/YGYTServer/util.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
+# coding:utf-8
+# Copyright (C) dirlt
 
 import config as CF
 import string
@@ -15,33 +15,41 @@ import gevent
 # gevent.queue
 import urlparse
 
-def parse_videos_csv(fname = 'videos.tsv'):
+
+def parse_videos_csv(fname='videos.tsv'):
     with open(fname) as fh:
         lines = map(lambda x: x.strip(), fh.readlines())
         fields = lines[0].split('\t')
         records = []
         for line in lines[1:]:
-            if line.startswith('#'): continue
+            if line.startswith('#'):
+                continue
             item = line.split('\t')
             record = {}
             lessons = []
             labels = []
             record['lessons'] = lessons
             record['labels'] = labels
-            for (i,v) in enumerate(item):
+            for (i, v) in enumerate(item):
                 key = fields[i]
-                if not v: continue
+                if not v:
+                    continue
                 if key.startswith('Label'):
                     labels.append(v)
                 elif key.startswith('Lesson'):
                     lessons.append(v)
                 else:
                     def format_key(key):
-                        if key == 'Title': return 'title'
-                        elif key == 'Author': return 'author'
-                        elif key == 'Playlist': return 'playlist'
-                        elif key == 'Language': return 'lang'
-                        else: return key
+                        if key == 'Title':
+                            return 'title'
+                        elif key == 'Author':
+                            return 'author'
+                        elif key == 'Playlist':
+                            return 'playlist'
+                        elif key == 'Language':
+                            return 'lang'
+                        else:
+                            return key
                     key2 = format_key(key)
                     record[key2] = v
             if 'playlist' in record:
@@ -51,7 +59,8 @@ def parse_videos_csv(fname = 'videos.tsv'):
                     if q.startswith('v') and not 'cover-id' in record:
                         record['cover-id'] = q[2:]
                     elif q.startswith('list='):
-                        record['playlist'] = 'https://www.youtube.com/playlist?%s' % (q)
+                        record[
+                            'playlist'] = 'https://www.youtube.com/playlist?%s' % (q)
             record['title'] = '%s - %s' % (record['title'], record['author'])
             assert('cover-id' in record)
             record['key'] = record['cover-id']
@@ -61,6 +70,7 @@ def parse_videos_csv(fname = 'videos.tsv'):
 
 import time
 
+
 def mongo_cache_func(table, key, f, arg):
     data = table.find_one({'key': key})
     # print 'key = %s, data = %s' % (key, data)
@@ -69,10 +79,12 @@ def mongo_cache_func(table, key, f, arg):
         return data
     data = f(arg)
     data['_expire'] = time.time() + CF.EXPIRE_INTERVAL
-    table.replace_one({'key': key}, data, upsert = True)
+    table.replace_one({'key': key}, data, upsert=True)
     return data
 
 import pafy
+
+
 def nc_video_info(video_id):
     print '  > process video(%s)' % (video_id)
     v = pafy.new(video_id)
@@ -97,7 +109,11 @@ def nc_video_info(video_id):
         streams.append(info)
     data['streams'] = streams
     return data
-def video_info(video_id): return mongo_cache_func(CF.VIDEO_TABLE, video_id, nc_video_info, video_id)
+
+
+def video_info(video_id):
+    return mongo_cache_func(CF.VIDEO_TABLE, video_id, nc_video_info, video_id)
+
 
 def nc_playlist_info(playlist_id):
     print '  > process playlist(%s)' % (playlist_id)
@@ -107,7 +123,11 @@ def nc_playlist_info(playlist_id):
     data['description'] = pl['description']
     data['items'] = map(lambda x: x['pafy'].videoid, pl['items'])
     return data
-def playlist_info(playlist_id): return mongo_cache_func(CF.VIDEO_TABLE, playlist_id, nc_playlist_info, playlist_id)
+
+
+def playlist_info(playlist_id):
+    return mongo_cache_func(CF.VIDEO_TABLE, playlist_id, nc_playlist_info, playlist_id)
+
 
 def process_record(record):
     print '> process record(%s)' % (record['cover-id'])
@@ -120,13 +140,15 @@ def process_record(record):
         record[x] = vi[x]
     vs = []
     for vid in record['lessons']:
-        if vid in CF.BANNED_VIDEO_IDS: continue
+        if vid in CF.BANNED_VIDEO_IDS:
+            continue
         try:
             vi = video_info(vid)
         except:
             continue
         vs.append(vi)
-        if not record['description']: record['description'] = vi['description']
+        if not record['description']:
+            record['description'] = vi['description']
     keywords = set()
     viewcount = 0
     for vi in vs:
@@ -146,6 +168,7 @@ def process_record(record):
     record['videos'] = nvs
     return record
 
+
 def create_tables():
     CF.PLAYLIST_TABLE.create_index('key')
     CF.VIDEO_TABLE.create_index('key')
@@ -161,12 +184,14 @@ db.fact.dropIndex('text_index')
 db.fact.createIndex({title:'text',labels_text:'text',keywords_text:'text'}, {weights: {title:10, labels_text:8, keywords_text:5}, name:'text_index'}, {'default_language':'english'}, {'language_override':'lang'})
 """
 
+
 def do():
     create_tables()
     records = parse_videos_csv()
     for record in records:
         data = process_record(record)
-        CF.FACT_TABLE.replace_one({'key':data['key']}, data, upsert = True)
+        CF.FACT_TABLE.replace_one({'key': data['key']}, data, upsert=True)
+
 
 def test():
     records = parse_videos_csv()
