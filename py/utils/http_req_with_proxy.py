4287dd89ffcfee112aa7184ee081dca7fc9d5799
yanz <dirtysalt1987@gmail.com>
2016-02-22 12:42:15 +0800

    x

--- py/utils/http_req_with_proxy.py
+++ py/utils/http_req_with_proxy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
+# coding:utf-8
+# Copyright (C) dirlt
 
-from gevent import monkey; monkey.patch_socket()
+from gevent import monkey
+monkey.patch_socket()
 import gevent
 import urllib2
 
@@ -10,17 +11,20 @@ import urllib2
 # remote_proxy = ('http', '124.206.164.180:3128')
 # anonymous
 # remote_proxy = ('http', '124.202.182.174:8118')
-remote_proxy = ('http', '52.2.32.237:3128')
+# remote_proxy = ('http', '52.2.32.237:3128')
+
+remote_proxy = ('http', '127.0.0.1:9000')
 
 # does not work.
 # local_proxy = ('socks5', '127.0.0.1:62221')
 
 req_url = 'http://www.baidu.com'
 
+
 def req_with_proxy_0(url, proxy):
     print 'r0 proxy = {} ...'.format(proxy)
     (type, addr) = proxy
-    proxy_handler = urllib2.ProxyHandler({type : addr})
+    proxy_handler = urllib2.ProxyHandler({type: addr})
     opener = urllib2.build_opener(proxy_handler)
     urllib2.install_opener(opener)
     response = urllib2.urlopen(url)
@@ -28,6 +32,7 @@ def req_with_proxy_0(url, proxy):
     response.close()
     print 'r0 proxy = {} done'.format(proxy)
     return ('r0', proxy, data)
+
 
 def req_with_proxy_1(url, proxy):
     print 'r1 proxy = {} ...'.format(proxy)
