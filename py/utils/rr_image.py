12e11d0a6b806e6951eab1f38a0d31e7ef38d37b
yanz <dirtysalt1987@gmail.com>
2016-05-22 16:09:06 +0800

    l

--- py/utils/rr_image.py
+++ py/utils/rr_image.py
@@ -9,174 +9,95 @@ import json
 import string
 import threading
 
-class Robot:
-    def __init__(self, baseUrl, pageNum, reObj, saved):
-        self.baseUrl = baseUrl
-        self.pageNum = pageNum
-        self.reObj = reObj
-        self.saved = saved
 
-    def ExtractLinks(self, htmlPage, reObj):
-        links = []
-        offset = 0
-        while True:
-            m = reObj.search(htmlPage, offset)
-            if m:
-                links.append(m.group(0))
-                offset = m.end() + 1
-            else:
-                break
-        return links
+import requests
+from bs4 import BeautifulSoup
+import hashlib
 
-    def getHTMLPage(self, url, retry = 3):
-        while True:
-            try:
-                fd = urllib2.urlopen(url)
-                html = fd.read()
-                return html
-            except e:
-                retry -= 1
-                if retry == 0:
-                    return None
+CACHE_DIR = 'cache/'
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
 
-    def run(self):
-        if (os.path.exists(self.saved)):
-            fp = open(self.saved)
-            cached = json.load(fp)
-            fp.close()
-        else:
-            cached = {}
+def get_url(url, ss = None):
+    if not ss:
+        ss = requests.session()
+    key = hashlib.md5(url).hexdigest()
+    cache = CACHE_DIR + key
+    if os.path.exists(cache):
+        data = open(cache).read()
+        return data
+    r = ss.get(url)
+    with open(cache, 'w') as fh:
+        fh.write(r.content)
+    return r.content
 
-        lks = []
-        for i in range(0, self.pageNum):
-            url = self.baseUrl + '?curpage=%d'%(i)
-            print 'visit url(%s) ...'%(url)
-            htmlPage = self.getHTMLPage(url)
-            links = self.ExtractLinks(htmlPage, self.reObj)
-            print 'visit url(%s) done'%(url)
-            lks.extend(links)
 
-        for lk in lks:
-            if not lk in cached:
-                cached[lk] = 0
+def get_album_urls(station_id = 600261907):
+    cache = CACHE_DIR + 'album.urls.txt'
+    if not os.path.exists(cache):
+        ss = requests.session()
+        urls = []
+        init_url = 'http://page.renren.com/%d/album' % station_id
 
-        fp = open(self.saved, 'w')
-        json.dump(cached, fp)
-        fp.close()
-        return cached.keys()
+        def f(url, init = False):
+            print('get url %s' % url)
+            data = get_url(url, ss)
+            bs = BeautifulSoup(data)
+            xs = bs.select('#tabBody > div > div > div > table > tbody > tr > td > div > a')
+            for x in xs:
+                path = x.attrs['href']
+                urls.append('http://page.renren.com' + path)
+            if init:
+                xs = bs.select('#tabBody > div > div > div > ol > li > a')
+                last_page = int(xs[-1].attrs['href'].split('=')[-1])
+                return last_page
 
-    def run2(self, urls):
-        assert(self.pageNum == 1)
-        if os.path.exists(self.saved):
-            fp = open(self.saved)
-            cached = json.load(fp)
-            fp.close()
-        else:
-            cached = {}
+        last_page = f(init_url, True)
+        for x in range(1, last_page + 1):
+            url = init_url + '?curpage=%d' % x
+            f(url)
 
-        def save():
-            fp = open(self.saved, 'w')
-            json.dump(cached, fp)
-            fp.close()
+        with open(cache, 'w') as fh:
+            fh.writelines(map(lambda x: x + '\n', urls))
+        return urls
 
-        cnt = 0
-        for url in urls:
-            if (url in cached):
-                print 'cached...'
-                continue
-            print 'visit url(%s) ...'%(url)
-            htmlPage = self.getHTMLPage(url)
-            links = self.ExtractLinks(htmlPage, self.reObj)
-            if len(links) == 0 : continue
-            lk = string.split(links[0], ':', maxsplit = 1)[1][1:-1]
-            lk = lk.replace('\\','')
-            print lk
-            cached[url]=lk
+    with open(cache) as fh:
+        return [x.strip() for x in fh]
 
-            # incremental saved.
-            cnt += 1
-            if ((cnt % 10) == 0):
-                save()
-        save()
-        return cached
 
-    def download(self, threadNum = 10):
-        fp = open(self.saved)
-        js = json.load(fp)
-        fp.close()
+def get_image_urls(album_url):
+    album_id = album_url.split('/')[-1]
+    cache = CACHE_DIR + 'image.urls.%s.txt' % album_id
+    if os.path.exists(cache):
+        with open(cache) as fh:
+            return [x.strip() for x in fh]
 
-        item = len(js.keys())
-        pool = []
-        for i in range(0, threadNum):
-            start = i * item / threadNum
-            end = (i + 1) * item / threadNum
-            if i == (threadNum - 1):
-                end = item - 1
-            pool.append(threading.Thread(target=wget_download,
-                        args=(js, start, end)))
-        for i in range(0, threadNum):
-            pool[i].start()
-        for i in range(0, threadNum):
-            pool[i].join()
+    curpage = 0
+    ss = requests.session()
+    images = []
+    while True:
+        url = album_url + '?curpage=%d' % curpage
+        print('get url %s' % url)
+        data = get_url(url, ss)
+        bs = BeautifulSoup(data)
+        xs = bs.select('#single-column > table > tbody > tr > td > a')
+        for x in xs:
+            images.append('http://page.renren.com' + x.attrs['href'])
+        # 最后一页
+        xs = bs.select('#content > div.pager-top > ol > li > a')
+        if not xs or 'class' not in xs[-1].attrs:
+            break
+        curpage += 1
 
-def wget_download(js, s, e):
-    def fixUrl2FileName(url):
-        if url.startswith('http://'):
-            url = url[len('http://'):]
-        url = url.replace('/','.')
-        return url + '.jpg' # is that ok ?
-
-    keys = js.keys()[s:e]
-    for k in keys:
-        fname = fixUrl2FileName(k)
-        fname = 'images/%s'%(fname)
-        if os.path.exists(fname):
-            print 'cached...'
-            continue
-        url = js[k]
-        cmd = "wget -O '%s' '%s'"%(fname, url)
-        print cmd
-        os.system(cmd)
-
-def createAlbumRobot(accNo = '600261907', pages = 7):
-    reObj = re.compile(r'/\d+/album/\d+')
-    return Robot('http://page.renren.com/%s/album'%(accNo), pages, reObj, 'album.list')
-
-def createPhotoRobot(album, pages = 10):
-    reObj = re.compile(r'/\d+/photo/\d+')
-    return Robot('http://page.renren.com' + album, pages, reObj, 'photo.list')
-
-def createPhotoLink():
-    fp = open('photo.list')
-    js = json.load(fp)
-    fp.close()
-    urls = map(lambda x : 'http://page.renren.com%s'%(x), js.keys())
-
-    reObj = re.compile(r'\"(largeurl|large)\":"[^"]+"')
-    robot = Robot('', 1, reObj, 'photo.link')
-    robot.run2(urls)
-
-def downloadPhoto():
-    robot = Robot('', '', '', 'photo.link')
-    robot.download()
+    with open(cache, 'w') as fh:
+        fh.writelines(map(lambda x: x + '\n', images))
+    return images
 
 if __name__ == '__main__':
-    _createPhotoList = True
-    _createPhotoLink = True
-    _downloadPhoto = True
-
-    if _createPhotoList :
-        # following action costs too much.
-        # better just run once
-        # download photo list.
-        robot = createAlbumRobot()
-        albums = robot.run()
-        for album in albums:
-            robot = createPhotoRobot(album)
-            robot.run()
-
-    if _createPhotoLink :
-        createPhotoLink()
-
-    if _downloadPhoto:
-        downloadPhoto()
+    urls = get_album_urls()
+    imgs = []
+    for url in urls:
+        images = get_image_urls(url)
+        imgs.extend(images)
+    with open('rr_image.urls.txt', 'w') as fh:
+        fh.writelines(map(lambda x: x + '\n', imgs))
