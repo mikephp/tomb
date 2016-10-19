e8975b639098699ece44d7861d6f88e9543f9ec2
yanz <dirtysalt1987@gmail.com>
2016-10-19 21:26:14 +0800

    x

--- py/youtube-api/search.py
+++ py/youtube-api/search.py
@@ -23,13 +23,41 @@ DEVELOPER_KEY = "AIzaSyBglP9GAV_zOD3LGhPoIWTAfsEzgAOJ4yU"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+import hashlib
+import cPickle as pickle
+import pandas as pd
+def get_sign(s):
+    if not isinstance(s, str):
+        s = pickle.dumps(s)
+    x = hashlib.sha1(s)
+    return x.hexdigest()
+
+import redis
+import json
+RedisClient = redis.StrictRedis(host = '127.0.0.1', port = 6379, db = 0)
+def read_cache(key):
+    key = 'yt.' + get_sign(key)
+    v = RedisClient.get(key)
+    return json.loads(v) if v is not None else None
+
+def write_cache(key, value, timeout = None):
+    key = 'yt.' + get_sign(key)
+    value = json.dumps(value)
+    if timeout is None:  RedisClient.set(key, value)
+    else: RedisClient.setex(key, timeout, value)
+
 def create_youtube_instance():
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
         developerKey=DEVELOPER_KEY)
     return youtube
+youtube = create_youtube_instance()
 
 def youtube_search(options):
-    youtube = options.youtube
+    cache_value = read_cache(options)
+    if cache_value is not None:
+        return cache_value
+
+    print('----- WORKING ON SEARCH -----')
     # Call the search.list method to retrieve results matching the specified
     # query term.
     search_response = youtube.search().list(
@@ -37,7 +65,8 @@ def youtube_search(options):
       part="id,snippet",
       videoDuration = options.videoDuration,
       # maxResults=options.max_results
-      maxResults = 25,
+      # don't change it, otherwise cache invalidates.
+      maxResults = options.numsPerPage,
       pageToken = options.pageToken,
       **{'type': 'video'}
     ).execute()
@@ -64,29 +93,73 @@ def youtube_search(options):
     # print "Videos:\n", "\n".join(videos), "\n"
     # print "Channels:\n", "\n".join(channels), "\n"
     # print "Playlists:\n", "\n".join(playlists), "\n"
-    return (search_response.get('items', []), search_response['pageInfo']['totalResults'], search_response['nextPageToken'])
+    res = {'items': search_response.get('items', []),
+            'nextPageToken': search_response.get('nextPageToken', None)}
+    write_cache(options, res)
+    return res
 
 def youtube_search_all(options):
     options.videoDuration = 'short'
     options.pageToken = ''
-    options.youtube = create_youtube_instance()
+    options.numsPerPage = 50
 
     n = options.max_results
     count = 0
+    results = []
     while count < n:
-        (items, totalNumber, nextPageToken) = youtube_search(options)
-        print 'totalNumber : ', totalNumber
-        print "Videos: \n", "\n".join(map(lambda x: x['snippet']['title'], items))
+        res = youtube_search(options)
+        items = res['items']
+        nextPageToken = res['nextPageToken']
+        if not len(items) or not nextPageToken:
+            break
+        # print "Videos: \n", "\n".join(map(lambda x: x['snippet']['title'], items))
         options.pageToken = nextPageToken
         count += len(items)
+        for x in items:
+            results.append({'videoId': x['id']['videoId'],
+                            'title': x['snippet']['title'].encode('utf8'),
+                            'description': x['snippet']['description'].encode('utf8')})
+    return results
+
+def youtube_videos_tags(ids):
+    results = {}
+    query_ids = []
+    for vid in ids:
+        k = 'video.' + vid
+        value = read_cache(k)
+        if value: results[vid] = value['snippet'].get('tags', [])
+        else: query_ids.append(vid)
+
+    idx = 0
+    while idx < len(query_ids):
+        print '----- WORKING ON TAGS -----'
+        sub_ids = query_ids[idx: idx + 50]
+        idx += 50
+        response = youtube.videos().list(
+            id = ','.join(sub_ids),
+            part = 'snippet').execute()
+        items = response['items']
+        for x in items:
+            vid = x['id']
+            k = 'video.' + vid
+            write_cache(k, x)
+            results[vid] = x['snippet'].get('tags', [])
+
+    return results
 
 if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
-  argparser.add_argument("--max-results", help="Max results", default=100)
-  args = argparser.parse_args()
+    argparser.add_argument("--q", help="Search term", default="Google")
+    argparser.add_argument('--output-file', help = "Output file", default = "output.csv")
+    argparser.add_argument("--max-results", help="Max results", default=100)
+    args = argparser.parse_args()
 
-  try:
-      # youtube_search(args)
-      youtube_search_all(args)
-  except HttpError, e:
-      print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+    try:
+        # youtube_search(args)
+        results = youtube_search_all(args)
+        tags = youtube_videos_tags(map(lambda x: x['videoId'], results))
+        for r in results:
+            r['tags'] = (', '.join(tags[r['videoId']])).encode('utf8')
+        df = pd.DataFrame(results, columns = ('videoId', 'title', 'description', 'tags'))
+        df.to_csv(args.output_file, index = False)
+    except HttpError, e:
+        print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
