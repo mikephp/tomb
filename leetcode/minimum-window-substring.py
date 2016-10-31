c28c37943df2db3ddf7e1b62cf70bf977f288c73
yanz <dirtysalt1987@gmail.com>
2016-10-31 10:08:06 +0800

    x

---
+++ leetcode/minimum-window-substring.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Copyright (C) dirlt
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+
+        
