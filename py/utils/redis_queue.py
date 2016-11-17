643be1c78f49e04183e0ef6176554b3224ec6d72
yanz <dirtysalt1987@gmail.com>
2016-11-17 12:43:54 +0800

    x

--- py/utils/redis_queue.py
+++ py/utils/redis_queue.py
@@ -3,8 +3,23 @@
 #Copyright (C) dirlt
 
 import redis
+import functools
+import time
 
 # http://peter-hoffmann.com/2012/python-simple-queue-redis-queue.html
+
+def _my_retry(wait_time = 5):
+    def fn(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            while True:
+                try:
+                    return f(*args, **kwargs)
+                except redis.connection.ConnectionError:
+                    print('redis conn error. reconnect it after %.2f seconds' % wait_time)
+                    time.sleep(wait_time)
+        return wrapper
+    return fn
 
 class RedisQueue(object):
     """Simple Queue with Redis Backend"""
@@ -13,18 +28,22 @@ class RedisQueue(object):
         self.__db= redis.Redis(**redis_kwargs)
         self.key = '%s:%s' %(namespace, name)
 
+    @_my_retry()
     def qsize(self):
         """Return the approximate size of the queue."""
         return self.__db.llen(self.key)
 
+    @_my_retry()
     def empty(self):
         """Return True if the queue is empty, False otherwise."""
         return self.qsize() == 0
 
+    @_my_retry()
     def put(self, item):
         """Put item into the queue."""
         self.__db.rpush(self.key, item)
 
+    @_my_retry()
     def get(self, block=True, timeout=None):
         """Remove and return an item from the queue.
 
@@ -39,6 +58,7 @@ class RedisQueue(object):
             item = item[1]
         return item
 
+    @_my_retry()
     def get_nowait(self):
         """Equivalent to get(False)."""
         return self.get(False)
